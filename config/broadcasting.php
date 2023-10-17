dd1f257957a7410e80e7ba62f9df70116b7cff6b
jose <jpadillayapura@gmail.com>
2023-10-17 02:14:03 -0400

    cambios

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
+                'useTLS' => true,
                 'host' => '3.128.184.156',
                 'port' => 6001,
                 'scheme' => 'http'
