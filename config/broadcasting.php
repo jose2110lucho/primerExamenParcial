113447f1279419f838ed75edcbf6fbfe18ccb7be
jose <jpadillayapura@gmail.com>
2023-10-17 12:03:28 -0400

    nada

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
                 'useTLS' => true,
-                'host' => '127.0.0.1',
+                'host' => '3.128.184.156',
                 'port' => 6001,
                 'scheme' => 'http'
             ],
